09fc4a5cd2b4295991f6d0ba880b6c0fe7d40255
JulienTHP <jpelisson@hotmail.fr>
2019-01-14 17:37:34 +0100

    this is the end game

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,37 +1,45 @@
-def echo(word)
-	return "#{word}"
-end
+def translate(phrase)
+  arr = phrase.split(' ')
+  arr2 = []
+  arr.each do |string|
 
-def shout(word)
-	return word.upcase
-end
 
-def repeat(word)
-	word = word + " " + word 
-	return word
-end
+    if string[0] == string[0].scan(/[aeiou]/).join 
+      string.insert(-1, "ay")
+      arr2 << string
+    end
+    if string[0 .. 1] == string[0 .. 1].scan(/[^aeiou][aeiou]/).join  && string[0 .. 1] != string[0 .. 1].scan(/[q][aeiou]/).join
+      string2 = string[0 .. 1].scan(/[^aeiou]/).join
+      string.delete_prefix!(string2).insert(-1, string2).insert(-1, "ay")
+      arr2 << string
+    end
 
-def repeat(word, num = 2)
-	word = [word] * num * ' '
-end
+    if string[0 .. 2] == string[0 .. 2].scan(/[^aeiou][^aeiou][aeiou]/).join && string[0 .. 2] != string[0 .. 2].scan(/[a-z][q][u]/).join
+      string2 = string[0 .. 2].scan(/[^aeiou][^aeiou]/).join
+      string.delete_prefix!(string2).insert(-1, string2).insert(-1, "ay")
+      arr2 << string
+    end
+    if string[0 .. 2] == string[0 .. 2].scan(/[^aeiou][^aeiou][^aeiou]/).join
+      string2 = string[0 .. 2].scan(/[^aeiou][^aeiou][^aeiou]/).join
+      string.delete_prefix!(string2).insert(-1, string2).insert(-1, "ay")
+      arr2 << string
+    end
+    if string[0 .. 2] == string[0 .. 2].scan(/[s][c][h]/).join
+      string2 = string[0 .. 2].scan(/[s][c][h]/).join
+      string.delete_prefix!(string2).insert(-1, string2).insert(-1, "ay")
+      arr2 << string
+    end
+    if string[0 .. 1] == string[0 .. 1].scan(/[q][u]/).join
+      string2 = string[0 .. 1].scan(/[q][u]/).join
+      string.delete_prefix!(string2).insert(-1, string2).insert(-1, "ay")
+      arr2 << string
+    end
+    if string[0 .. 2] == string[0 .. 2].scan(/[^aeiou][q][u]/).join
+      string2 = string[0 .. 2].scan(/[^aeiou][q][u]/).join
+      string.delete_prefix!(string2).insert(-1, string2).insert(-1, "ay")
+      arr2 << string
+    end
 
-def start_of_word(word, num = 1)
-	num = num - 1
-	return word[num]
-end
-
-def start_of_word(word, num = 2)
-	return word[0,num]
-end
-
-def first_word(word)
- 	return	word.split.first
- end
-
- def titleize(word)
- 	 word.split(/ |\_/).map(&:capitalize).join(" ")
- end
-
-def title_case(wordArray)
-
+  end
+  return arr2.join(" ")
 end
